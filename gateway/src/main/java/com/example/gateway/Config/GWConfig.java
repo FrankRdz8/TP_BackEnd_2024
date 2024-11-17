9a1f3b93b84a06da9bd1d99b17736619755e4769
FrankRdz8 <frankrdz8@protonmail.com>
2024-11-17 18:20:06 -0300

    Gateway y security

--- gateway/src/main/java/com/example/gateway/Config/GWConfig.java
+++ gateway/src/main/java/com/example/gateway/Config/GWConfig.java
@@ -10,15 +10,16 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 @Configuration
 public class GWConfig {
 
+    // Empleado crea pruebas y manda notificaciones
+    // Vehiculo envia posiciones
+    // Administrador ver reportes
     @Bean
     public RouteLocator configureRoutes(RouteLocatorBuilder builder,
-                                        @Value("${gateway.url.microservicio.notificaciones}") String uriNotificaciones,
-                                        @Value("${gateway.url.microservicio.tpi_grupo58}") String uriTpiGrupo58) {
+                                        @Value("${gateway.url-microservicio-notificaciones}") String uriNotificaciones,
+                                        @Value("${gateway.url-microservicio-tpi_grupo58}") String uriTpiGrupo58) {
         return builder.routes()
-                // Rutas hacia el microservicio de notificaciones
-                .route("notificaciones_route", r -> r.path("/api/notificaciones/").uri(uriNotificaciones))
-                // Rutas hacia el microservicio principal (tpi_grupo58)
-                .route("principal_route", r -> r.path("/api/").uri(uriTpiGrupo58))
+                .route(r -> r.path("/api/notificaciones/**").uri(uriNotificaciones))
+                .route(r -> r.path("/api/tpi_grupo58/**").uri(uriTpiGrupo58))
                 .build();
     }
 }
